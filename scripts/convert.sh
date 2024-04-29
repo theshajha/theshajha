9c350e1005e1a24605456391cf686d05251cf855
Shashank Jha <shashankjha1994@gmail.com>
2024-04-29 22:43:09 +0530

    testing posts

--- scripts/convert.sh
+++ scripts/convert.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-# Navigate to the script's directory
+# Change directory to the current script's directory
 cd "$(dirname "$0")"
 
 # Install Python dependencies
-pip install markdown beautifulsoup4
+echo "Installing dependencies..."
+pip install markdown
 
-# Run the Python Markdown to HTML conversion script
+# Run the Markdown to HTML conversion script
+echo "Running conversion script..."
 python md_to_html.py
+
+echo "Conversion completed."
--- scripts/md_to_html.py
+++ scripts/md_to_html.py
@@ -1,36 +1,28 @@
 import os
-from markdown import markdown
-from bs4 import BeautifulSoup
+import markdown
 
 # Paths relative to the script location
-script_dir = os.path.dirname(__file__)
+script_dir = os.path.dirname(os.path.realpath(__file__))
 markdown_dir = os.path.join(script_dir, '../pages/posts/markdown/')
 html_dir = os.path.join(script_dir, '../pages/posts/')
 
-# Ensure output directory exists
-if not os.path.exists(html_dir):
-    os.makedirs(html_dir)
+# Ensure the HTML directory exists
+os.makedirs(html_dir, exist_ok=True)
 
 # Convert each Markdown file to HTML
 for md_filename in os.listdir(markdown_dir):
     if md_filename.endswith('.md'):
+        md_filepath = os.path.join(markdown_dir, md_filename)
+        html_filename = os.path.splitext(md_filename)[0] + '.html'
+        html_filepath = os.path.join(html_dir, html_filename)
+
         # Read the Markdown file
-        with open(os.path.join(markdown_dir, md_filename), 'r', encoding='utf-8') as md_file:
-            md_content = md_file.read()
-
-        # Convert to HTML
-        html_content = markdown(md_content)
-
-        # Optional: Prettify the HTML
-        soup = BeautifulSoup(html_content, 'html.parser')
-        pretty_html = soup.prettify()
-
-        # Define the HTML file name
-        base = os.path.splitext(md_filename)[0]
-        html_filename = base + '.html'
+        with open(md_filepath, 'r', encoding='utf-8') as file:
+            text = file.read()
+            html = markdown.markdown(text)
 
         # Write the HTML content to a file
-        with open(os.path.join(html_dir, html_filename), 'w', encoding='utf-8') as html_file:
-            html_file.write(pretty_html)
+        with open(html_filepath, 'w', encoding='utf-8') as file:
+            file.write(html)
 
         print(f'Converted {md_filename} to {html_filename}')
