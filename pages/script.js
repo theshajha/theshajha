a136c187f95e6246effcaf120248e56d556d5054
Shashank Jha <shashankjha1994@gmail.com>
2024-04-29 21:12:43 +0530

    Adding blog posts

--- pages/script.js
+++ pages/script.js
@@ -1,7 +1,26 @@
 document.addEventListener("DOMContentLoaded", function () {
   const contentElement = document.getElementById("content");
-  const markdownContent = `# Your Markdown Here
-Replace this with your actual markdown content from your README.md file.`;
+  let path = window.location.pathname.split("/").pop();
 
-  contentElement.innerHTML = marked(markdownContent);
+  if (path) {
+    path = path.replace(/-/g, "_") + ".md"; // Converts the slug to the filename format and appends .md
+    fetch(`./posts/${path}`)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        return response.text();
+      })
+      .then((text) => {
+        contentElement.innerHTML = marked(text);
+      })
+      .catch((e) => {
+        console.error("Error fetching the Markdown file: ", e);
+        contentElement.innerHTML =
+          "<p>Error loading content. Please try again later.</p>";
+      });
+  } else {
+    contentElement.innerHTML =
+      "<p>Welcome to my blog! Please click on a post to read.</p>";
+  }
 });
