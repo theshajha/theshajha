8efaa9f3f38618abf0b9cad139610f60ab4e691a
Shashank Jha <shashankjha1994@gmail.com>
2024-04-29 21:22:48 +0530

    Adding blog posts

--- pages/script.js
+++ pages/script.js
@@ -1,34 +1,32 @@
 document.addEventListener("DOMContentLoaded", function () {
   const contentElement = document.getElementById("content");
-  // Use 'location.href' to handle full URL paths and potential redirects
-  let path = window.location.href;
 
-  // Extracts the slug from the URL assuming the URL pattern is:
-  // "https://theshajha.com/blog/i-turned-28-today/" or
-  // "https://theshajha.github.io/theshajha/blog/i-turned-28-today/"
-  let slug = path.match(/blog\/([^\/]+)\/?$/);
+  // Getting the full URL and then parsing out just the part after the last slash if it exists
+  let fullUrl = window.location.href;
+  let matches = fullUrl.match(/\/blog\/([^\/?#]+)\/?$/);
 
-  if (slug && slug[1]) {
-    // Replace dashes with underscores and append '.md' to form the filename
-    let filename = slug[1].replace(/-/g, "_") + ".md";
-    // Construct the path to the Markdown file within the 'posts' directory
-    fetch(`./posts/${filename}`)
+  if (matches && matches[1]) {
+    let postSlug = matches[1]; // This is the slug, e.g., "i-turned-28-today"
+
+    // Replace '-' with '_' to match the file naming convention
+    let markdownFileName = postSlug.replace(/-/g, "_") + ".md";
+    let markdownFilePath = `./posts/${markdownFileName}`;
+
+    fetch(markdownFilePath)
       .then((response) => {
         if (!response.ok) {
-          throw new Error(`HTTP error! status: ${response.status}`);
+          throw new Error(`HTTP error! Status: ${response.status}`);
         }
         return response.text();
       })
       .then((text) => {
-        // Use the 'marked' library to convert Markdown to HTML
         contentElement.innerHTML = marked(text);
       })
       .catch((e) => {
         console.error("Error fetching the Markdown file: ", e);
-        contentElement.innerHTML = `<p>Error loading the blog post. Please try again later.</p>`;
+        contentElement.innerHTML = `<p>Error loading the blog post. Please check the URL or try again later.</p>`;
       });
   } else {
-    // Default message when no specific blog post slug is found in the URL
     contentElement.innerHTML = `<p>Welcome to my blog! Please click on a post to read.</p>`;
   }
 });
